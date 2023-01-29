752f5809556d033c8bc28edbe209d5a5163b72f5
haonhat0703 <haonhat2729@gmail.com>
2023-01-29 20:50:14 +0700

    say hello with me

--- function/auto.py
+++ function/auto.py
@@ -1,7 +1,40 @@
-import pyautogui
+import pyautogui as pg
 import time
-while True:
-    time.sleep(4)
-    pyautogui.typewrite('Hello! Motherfu**ing :D')
-    time.sleep(2)
-    pyautogui.press('enter')
+import webbrowser
+# while True:
+#     time.sleep(4)
+#     pyautogui.typewrite('Hello! Motherfu**ing :D')
+#     time.sleep(2)
+#     pyautogui.press('enter')
+
+# time.sleep(2)
+# print(pg.position())
+# pg.moveTo(562, 755, 2)
+# pg.leftClick()
+
+url = "https://www.facebook.com/messages/t/100017290625742"
+webbrowser.get().open(url)
+print(pg.position())
+# pg.moveTo(970, 1079, 2)
+# pg.moveTo(1026, 1052, 2)
+# pg.leftClick(1026, 1052, 1)
+# pg.keyDown('ctrl')
+# pg.press('t')
+# pg.keyUp('ctrl')
+#     pg.moveTo(661, 479)
+#     pg.leftClick()
+#     pg.typewrite("hello bạn")
+#     pg.press('enter')
+time.sleep(10)
+
+for i in range(6):
+    pg.keyDown('alt')
+    for j in range(i):
+        pg.press('tab')
+    pg.press('enter')
+    pg.keyUp('alt')
+    pg.moveTo(959, 1026)
+    pg.leftClick()
+    pg.typewrite("hello bạn")
+    pg.press('enter')
+    #pg.hotkey('alt', 'tab', 'enter')
